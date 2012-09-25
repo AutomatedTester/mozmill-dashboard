a85cfb1a1a9a0a6eadfbdd01da5fc49aba739491
David Burns <david.burns@theautomatedtester.co.uk>
2012-09-25 05:25:28 -0700

    adding a basic test to check the site doesnt crash when we add filters

--- apps/display/test/test_display.py
+++ apps/display/test/test_display.py
@@ -10,3 +10,14 @@ class DisplayResults(test_utils.TestCase):
                                 'display/reports/reports.html')
         self.assertEquals(0, len(response.context['reports'].values_list()))
 
+    def test_that_filters_dont_create_500(self):
+        response = self.client.get('/en-US/', {'os': 'all', 
+                                                'locale':'en-US', 
+                                                'to_date': '2012-09-12',
+                                                'from_date': '2012-09-14'})
+        self.assertEqual(200, response.status_code)
+        self.assertTemplateUsed(response, 
+                                'display/reports/reports.html')
+        self.assertEquals(0, len(response.context['reports'].values_list()))
+
+
