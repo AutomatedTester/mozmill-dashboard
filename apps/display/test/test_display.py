00e4411da588ecea05ef8723feb54451b2222e71
David Burns <david.burns@theautomatedtester.co.uk>
2012-09-25 06:14:42 -0700

    adding a fixture and updating test accordingly

--- apps/display/test/test_display.py
+++ apps/display/test/test_display.py
@@ -3,12 +3,14 @@ import test_utils
 
 class DisplayResults(test_utils.TestCase):
     
+    fixtures = ['display_views_testdata.json']
+
     def test_that_we_can_display_all_results(self):
         response = self.client.get("/en-US/")
         self.assertEqual(200, response.status_code)
         self.assertTemplateUsed(response, 
                                 'display/reports/reports.html')
-        self.assertEquals(0, len(response.context['reports'].values_list()))
+        self.assertEquals(2, len(response.context['reports'].values_list()))
 
     def test_that_filters_dont_create_500(self):
         response = self.client.get('/en-US/', {'os': 'all', 
