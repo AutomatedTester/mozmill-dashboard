1984d5729305db7b49df61a18510cc72d961eae1
AutomatedTester <dburns@mozilla.com>
2012-10-03 16:35:57 +0100

    adding puppet scripts

---
+++ puppet/manifests/vagrant.pp
@@ -0,0 +1,27 @@
+#
+# Playdoh puppet magic for dev boxes
+#
+import "classes/*.pp"
+
+$PROJ_DIR = "/home/vagrant/project"
+
+# You can make these less generic if you like, but these are box-specific
+# so it's not required.
+$DB_NAME = "dashboard"
+$DB_USER = "root"
+
+Exec {
+    path => "/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin",
+}
+
+class dev {
+    class {
+        init: before => Class[mysql];
+        mysql: before  => Class[python];
+        python: before => Class[apache];
+        apache: before => Class[playdoh];
+        playdoh: ;
+    }
+}
+
+include dev
